4dc3c6be684ba9123785164a6f9a836392b864ba
nannu-coder <nazmulnannu43@gmail.com>
2022-01-01 17:59:48 +0600

    finished all the site

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,149 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
+import { FiTwitter, FiFacebook, FiInstagram, FiYoutube, FiPhoneCall } from 'react-icons/fi';
+import { FaWhatsapp } from 'react-icons/fa';
+import { GoLocation } from 'react-icons/go';
+import { BiEnvelope, BiTimeFive } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
+import img1 from '../../Images/packages/p-6.png';
+import img2 from '../../Images/packages/p-7.png';
+import img3 from '../../Images/packages/p-8.png';
+import img4 from '../../Images/packages/package1.png';
+import img5 from '../../Images/packages/package2.png';
+import img6 from '../../Images/packages/package3.png';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
+        <div className='footer'>
+            <div className="footer_top">
+                <Container>
+                    <Row>
+                        <div className="col-lg-3">
+                            <div className="footer_heading">
+                                <h1>About Us</h1>
 
+                                <p>“Travel is the main thing you purchase that makes you more extravagant”. We, at ‘Organization Name’, swear by this and put stock in satisfying travel dreams that make you perpetually rich constantly</p>
+                            </div>
+                            <div className="footer_icon">
+                                <ul>
+                                    <li>
+                                        <Link to='/coomsoon'>
+                                            <FiInstagram />
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to='/coomsoon'>
+                                            <FiTwitter />
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to='/coomsoon'>
+                                            <FiFacebook />
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to='/coomsoon'>
+                                            <FiYoutube />
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to='/coomsoon'>
+                                            <FaWhatsapp />
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-3">
+                            <div className="footer_heading">
+                                <h1>Get In Touch</h1>
+                            </div>
+                            <div className="contact_info">
+                                <ul>
+                                    <li>
+                                        <span><GoLocation /></span>
+                                        123 Street, New York, USA
+                                    </li>
+                                    <li>
+                                        <span><FiPhoneCall /></span>
+                                        +012 345 67890
+                                    </li>
+                                    <li>
+                                        <span><BiEnvelope /></span>
+                                        info@travelpro.com
+                                    </li>
+                                    <li>
+                                        <span><BiTimeFive /></span>
+                                        Mon-Sun 9am-10pm
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-2">
+                            <div className="footer_heading">
+                                <h1>Useful links</h1>
+                            </div>
+                            <div className="links">
+                                <ul>
+                                    <li>
+                                        <Link to='/coomsoon'>
+                                            New Destination
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to='/allpackage'>
+                                            All Services
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to='/coomsoon'>
+                                            Holiday package
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to='/coomsoon'>
+                                            Family Package
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+                            <div className="footer_heading">
+                                <h1 className='text-center'>Gallery</h1>
+                            </div>
+                            <div className="gallery">
+                                <Row>
+                                    <div className="col-lg-4 mb-4">
+                                        <img className='img-fluid w-100' src={img1} alt="" />
+                                    </div>
+                                    <div className="col-lg-4">
+                                        <img className='img-fluid w-100' src={img2} alt="" />
+                                    </div>
+                                    <div className="col-lg-4">
+                                        <img className='img-fluid w-100' src={img3} alt="" />
+                                    </div>
+                                    <div className="col-lg-4">
+                                        <img className='img-fluid w-100' src={img4} alt="" />
+                                    </div>
+                                    <div className="col-lg-4">
+                                        <img className='img-fluid w-100' src={img5} alt="" />
+                                    </div>
+                                    <div className="col-lg-4">
+                                        <img className='img-fluid w-100' src={img6} alt="" />
+                                    </div>
+                                </Row>
+                            </div>
+                        </div>
+                    </Row>
+                </Container>
+            </div>
+            <div class="footer_bottom text-center">
+                <Container>
+                    <p>Copyright 2021 TravelPro | Design By Nannu</p>
+                </Container>
+            </div>
         </div>
     );
 };
--- src/Components/HomePage/AboutUs/AboutUs.js
+++ src/Components/HomePage/AboutUs/AboutUs.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import about from '../../../Images/about.jpg'
 
 const AboutUs = () => {
     return (
-        <div className='mt-5'>
-            <h1>This Is About Page</h1>
+        <div style={{ 'marginTop': '65px' }} className='about_us'>
+            <Container>
+                <Row>
+                    <div class="about_heading text-center">
+                        <h1>About Us</h1>
+                    </div>
+                    <div class="about_img">
+                        <img className='img-fluid w-100' src={about} alt="" />
+                    </div>
+                    <div class="about_content mt-4">
+
+                        <h6>‘TravelPro’</h6>
+                        <p>
+                            “Travel is the main thing you purchase that makes you more extravagant”. We, at ‘TravelPro’, swear by this and put stock in satisfying travel dreams that make you perpetually rich constantly.
+                            <br /> <br />
+                            We have been moving excellent encounters for a considerable length of time through our cutting-edge planned occasion bundles and other fundamental travel administrations. We rouse our clients to carry on with a rich life, brimming with extraordinary travel encounters.</p>
+
+                        <p>Through our exceptionally curated occasion bundles, we need to take you on an adventure where you personally enjoy the stunning magnificence of America and far-off terrains. We need you to observe sensational scenes that are a long way past your creative ability.
+                            <br /> <br />
+                            The powerful inclination of American voyagers to travel more nowadays is something that keeps us inspired to satisfy our vacation necessities. Our vision to give you a consistent occasion encounter makes us one of the main visit administrators in the regularly extending travel industry.</p>
+
+
+                        <p>To guarantee that you have a satisfying occasion and healthy encounters, all our vacation administrations are available to your no matter what. On your universal occasion, we guarantee that you are very much outfitted with outside trade (Forex Cards, Currency Notes), visa, and travel protection.
+                            <br /><br />
+                            We are the pioneers of outside trade in America and booking forex online is basic and advantageous with us.
+                            <br /><br />
+                            Our online visa administrations are exceptional and make the bulky procedure of booking visas a cake stroll for clients. We likewise give exceptional visa, forex, and travel protection and outside settlement administrations for understudies voyaging abroad for study.
+                            <br /><br />
+                            Regardless of whether it’s reserving flights or inns for your movement, COmpany Name offers everything under one umbrella. We likewise have journey occasions for individuals who are searching for solace and reasonable extravagance.
+                            <br /><br />
+                            We offer the best limits on our top-rated visit bundles to clients who pick our viable administrations over and over. How about we remind you indeed that we don’t expect to be your visit and travel specialists; we endeavor to be your vacation accomplices until the end of time.
+
+                        </p>
+                    </div>
+                </Row>
+            </Container>
         </div>
     );
 };
